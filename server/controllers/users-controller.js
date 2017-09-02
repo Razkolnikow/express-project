55f74ab3c9923bb0970e3746d6d860708c087cc9
Raskolnikov <hristov.pavel88@gmail.com>
2017-09-02 19:29:15 +0300

    register functionality

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -1,4 +1,5 @@
 let encryption = require('../utilities/encryption');
+let User = require('mongoose').model('User');
 
 module.exports = {
   register: (req, res) => {
@@ -6,13 +7,22 @@ module.exports = {
   },
   create: (req, res) => {
     let user = req.body;
-    user.globalError = 'Passwords do not match!';
 
     if (user.password !== user.confirmPassword) {
+      user.globalError = 'Passwords do not match!';
       res.render('users/register', user)
     } else {
       user.salt = encryption.generateSalt();
       user.hashedPass = encryption.generateHashedPassword(user.salt, user.password);
+      User.create(user)
+        .then(user => {
+          req.logIn(user, (err, user) => {
+            if (err) {
+              res.render('users/register', { globalError: '500' })
+            }
+          })
+        });
+        
       res.redirect('/');
     }
   }
